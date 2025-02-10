c9eb39d15103fd1a4672529bc4dd24faa88dc265
sahil0989 <sahilbarak111@gmail.com>
2025-02-10 20:01:49 +0530

    port

--- backend/socket/index.js
+++ backend/socket/index.js
@@ -12,7 +12,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: "https://mohmaya-chat.vercel.app/",
+        origin: "https://mohmaya-chat.vercel.app:10000",
         credentials: true
     }
 })
